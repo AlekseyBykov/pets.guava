7852ad2f8e59c1e42b641908826a70afd4f50eb0
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-20 07:52:51 +0400

    Added test for example of using Splitter.

---
+++ src/test/java/alekseybykov/portfolio/guava/splitters/SplitterTest.java
@@ -0,0 +1,33 @@
+package alekseybykov.portfolio.guava.splitters;
+
+import com.google.common.base.Splitter;
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+public class SplitterTest {
+
+	private static String string;
+
+	@BeforeClass
+	public static void setup() {
+		string = " string 1|string 2|string 3|string 4 ";
+	}
+
+	@Test
+	public void testSplitStringToIterable() {
+		Splitter splitter = Splitter.on("|").trimResults();
+		Iterable<String> strings = splitter.split(string);
+
+		assertThat(strings, Matchers.<String> iterableWithSize(4));
+		assertThat(strings, everyItem(not(StringUtils.EMPTY)));
+
+		assertThat(strings, hasItems("string 1", "string 2", "string 3", "string 4"));
+	}
+}
