b2b720defd24d8e079821193e0efba594c4eef3a
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-20 20:55:40 +0400

    Added test for example of using MapSplitter.

---
+++ src/test/java/alekseybykov/portfolio/guava/splitters/MapSplitterTest.java
@@ -0,0 +1,36 @@
+package alekseybykov.portfolio.guava.splitters;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Maps;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MapSplitterTest {
+
+	private Map<String, String> map;
+
+	@Before
+	public void setup() {
+		map = Maps.newLinkedHashMap();
+	}
+
+	@Test
+	public void testSplitStringByKeyValuePairsToMap() {
+		map = Maps.newLinkedHashMap();
+		map.put("key 1", "value 1");
+		map.put("key 2", "value 2");
+		map.put("key 3", "value 3");
+
+		String string = "key 1=value 1#key 2=value 2#key 3=value 3";
+		Splitter.MapSplitter mapSplitter = Splitter.on("#").withKeyValueSeparator("=");
+
+		Map<String, String> actualMap = mapSplitter.split(string);
+
+		assertThat(actualMap, is(map));
+	}
+}
