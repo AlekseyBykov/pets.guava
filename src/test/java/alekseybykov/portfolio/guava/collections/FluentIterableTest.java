c6633f99cebd737d53c61d09ae3b0d11a8688bc7
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-30 15:20:54 +0400

    Added test for example of using mapping operation.

--- src/test/java/alekseybykov/portfolio/guava/collections/FluentIterableTest.java
+++ src/test/java/alekseybykov/portfolio/guava/collections/FluentIterableTest.java
@@ -1,6 +1,8 @@
 package alekseybykov.portfolio.guava.collections;
 
 import alekseybykov.portfolio.guava.model.Book;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
 import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
@@ -33,7 +35,7 @@ public class FluentIterableTest {
 	}
 
 	@Test
-	public void testChainFilterWithFrom() {
+	public void testFilteringBooksWithUsingChains() {
 		Iterable<Book> booksFilteredByPages = FluentIterable
 			.from(bookList)
 			.filter(new Predicate<Book>() {
@@ -45,5 +47,23 @@ public class FluentIterableTest {
 
 		assertThat(Iterables.contains(booksFilteredByPages, books[0]), is(true));
 		assertThat(Iterables.contains(booksFilteredByPages, books[2]), is(true));
+	}
+
+	@Test
+	public void testTransformList2ListWithUsingChains() {
+		List<String> transformedList = FluentIterable
+			.from(bookList)
+			.transform(new Function<Book, String>() { // mapping
+				@Override
+				public String apply(Book book) {
+					return Joiner.on(":").join(book.getPages(), book.getPrice());
+				}
+			}).toList();
+
+		assertThat(transformedList.get(0), is("10:20.2"));
+		assertThat(transformedList.get(1), is("5:10.99"));
+		assertThat(transformedList.get(2), is("11:1.44"));
+		assertThat(transformedList.get(3), is("3:8.4"));
+		assertThat(transformedList.get(4), is("1:7.3"));
 	}
 }
