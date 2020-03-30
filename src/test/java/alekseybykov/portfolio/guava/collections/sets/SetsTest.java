7b4106d5cdca267df58b5fda7e9aecebcf893b81
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-30 16:33:07 +0400

    Added tests suite for examples of using Sets utility.

---
+++ src/test/java/alekseybykov/portfolio/guava/collections/sets/SetsTest.java
@@ -0,0 +1,39 @@
+package alekseybykov.portfolio.guava.collections.sets;
+
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+public class SetsTest {
+
+	private static final Set<Integer> firstSet = Sets.newHashSet(1, 2, 3, 4, 5);
+	private static final Set<Integer> secondSet = Sets.newHashSet(3, 4, 5, 6, 7);
+
+	@Test
+	public void testDifference() {
+		assertThat(Sets.difference(firstSet, secondSet), hasItems(1, 2));
+		assertThat(Sets.difference(secondSet, firstSet), hasItems(6, 7));
+	}
+
+	@Test
+	public void testSymmetricDifference() {
+		assertThat(Sets.symmetricDifference(firstSet, secondSet), hasItems(1, 2, 6, 7));
+		assertThat(Sets.symmetricDifference(secondSet, firstSet), hasItems(1, 2, 6, 7));
+	}
+
+	@Test
+	public void testIntersection() {
+		assertThat(Sets.intersection(firstSet, secondSet), hasItems(3, 4, 5));
+		assertThat(Sets.intersection(secondSet, firstSet), hasItems(3, 4, 5));
+	}
+
+	@Test
+	public void testUnion() {
+		assertThat(Sets.union(firstSet, secondSet), hasItems(1, 2, 3, 4, 5, 6, 7));
+		assertThat(Sets.union(secondSet, firstSet), hasItems(1, 2, 3, 4, 5, 6, 7));
+	}
+}
